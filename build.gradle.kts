3bda8277ea6ad65c4dce4dca07b74c158fd12e9a
Bilal Haider <bhdevelopment92@gmail.com>
2022-02-14 10:33:49 +0000

    Added clientID and Secret to app

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.1.0'
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31'
+        classpath("com.android.tools.build:gradle:7.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
